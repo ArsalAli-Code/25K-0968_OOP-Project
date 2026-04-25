ae382b8ebd70b640ef35043f3f6da7f58b11d7fc
Arsal Ali <k250968@nu.edu.pk>
2026-04-26 00:34:17 +0500

    Add files via upload

---
+++ headers/Combat.h
@@ -0,0 +1,16 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+#include "Player.h"
+#include "Enemy.h"
+
+class CombatManager 
+{
+private:
+    void drawBorder() const; 
+
+public:
+    bool startFight(Player& p, Enemy* e);
+};
+
+#endif
